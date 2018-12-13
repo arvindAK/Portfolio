0d18fc093a7cfb70e557286240a719c813a1c7fa
Anurag Kumar <anuragak@users.noreply.github.com>
2018-12-13 07:58:26 +0000

    Updates

---
+++ precache-manifest.600306afaf5d8143bebf2dba16adb473.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "86bc52f6ddc9f226d076f8ea0974f28f",
+    "url": "/portfolio/static/media/2016.86bc52f6.png"
+  },
+  {
+    "revision": "ca61facfdf9538f66806",
+    "url": "/portfolio/static/css/main.e1fb8676.chunk.css"
+  },
+  {
+    "revision": "f68dcc1a0c0c6e129be0",
+    "url": "/portfolio/static/js/1.f68dcc1a.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "65503ea2eaa26586b6180cdc7526484a",
+    "url": "/portfolio/static/media/time.65503ea2.png"
+  },
+  {
+    "revision": "541cdd038f3fbfe7acc39b7abc5e2fb8",
+    "url": "/portfolio/static/media/2014.541cdd03.png"
+  },
+  {
+    "revision": "0dd973c7cc329e9ac6f933ef5e992fd6",
+    "url": "/portfolio/static/media/2014Hover.0dd973c7.png"
+  },
+  {
+    "revision": "ca61facfdf9538f66806",
+    "url": "/portfolio/static/js/main.ca61facf.chunk.js"
+  },
+  {
+    "revision": "7e8482cb238b1f8349d0b17283932b0c",
+    "url": "/portfolio/static/media/2016Hover.7e8482cb.png"
+  },
+  {
+    "revision": "b7fd8bda88fa5d92b3af99b936290196",
+    "url": "/portfolio/static/media/2018.b7fd8bda.png"
+  },
+  {
+    "revision": "131eccf244a097a57e7f3d668e9b9cf9",
+    "url": "/portfolio/static/media/2018Hover.131eccf2.png"
+  },
+  {
+    "revision": "cabe9119b1485740f0b7f45543d6a9c6",
+    "url": "/portfolio/static/media/now.cabe9119.png"
+  },
+  {
+    "revision": "abf8406efe4ba792025f332e4b7d5b4a",
+    "url": "/portfolio/static/media/nowHover.abf8406e.png"
+  },
+  {
+    "revision": "cc3174405fcc40345ff0a777b4d7006f",
+    "url": "/portfolio/static/media/portBG.cc317440.png"
+  },
+  {
+    "revision": "998f9002d7a692176618086adfead11c",
+    "url": "/portfolio/index.html"
+  }
+];
