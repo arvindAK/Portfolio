9b1b9f1558214c2ff0a2648985d38e7e4cc8a5c4
Anurag Kumar <anuragak@users.noreply.github.com>
2019-05-05 19:00:37 +0100

    Updates

---
+++ precache-manifest.500ba54d6fc0ffea862deaba24afc5c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dc0cea73f3f849c898ff4da89503630",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "ee46de14cfcb9c6a9a79",
+    "url": "/Portfolio/static/css/main.97c4cc99.chunk.css"
+  },
+  {
+    "revision": "04282861508b0ac2c77b",
+    "url": "/Portfolio/static/js/2.aaecb64b.chunk.js"
+  },
+  {
+    "revision": "ee46de14cfcb9c6a9a79",
+    "url": "/Portfolio/static/js/main.1a3b2b4a.chunk.js"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  },
+  {
+    "revision": "f91ae2ffc6bde933e0eda4fbfef13408",
+    "url": "/Portfolio/static/media/bike.f91ae2ff.png"
+  },
+  {
+    "revision": "cc3174405fcc40345ff0a777b4d7006f",
+    "url": "/Portfolio/static/media/portBG.cc317440.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.1048124d371ea13a0975e9c8c4753593.js"
+  "/Portfolio/precache-manifest.500ba54d6fc0ffea862deaba24afc5c5.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Portfolio/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
